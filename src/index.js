6eb6e89846bccac3d5a547fdb27a1356c8309262
Ferenandios <www.terunplay@gmail.com>
2023-08-16 20:42:14 +0500

    Store connected

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-// https://api.unsplash.com/photos/random?client_id=${CLIENT_ID}&count=${COUNT}
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
+  <Provider store={store}>
     <App />
-  </>
+  </Provider>
 );
