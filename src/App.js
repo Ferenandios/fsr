7c7ca481f4352488781480019b2f13d334530a10
Ferenandios <www.terunplay@gmail.com>
2023-08-16 20:44:49 +0500

    Created reject modal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import { useSelector } from "react-redux";
 import Button from "./components/Button/Button";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Items from "./components/Items/Items";
+import Reject from "./components/Reject/Reject";
 
 function App() {
+  const isReject = useSelector(state => state.photos.isReject)
   return (
     <main>
       <Header />
       <Items />
-      <Button />
-      <Footer />
+      {isReject ? <Reject /> : <Button />}
+      {!isReject && <Footer />}
     </main>
   );
 }
