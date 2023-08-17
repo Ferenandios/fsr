868c4e34c645c4201823fdcd1c610322f7ca1b08
Ferenandios <www.terunplay@gmail.com>
2023-08-17 13:34:19 +0500

    Added dispatch for search input and button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { BsSearch } from 'react-icons/bs'
 import './Header.css'
+import { useDispatch } from 'react-redux'
+import { getSearchPhotos } from '../../features/photos.slice'
 
 const Header = () => {
+  const inputRef = useRef('')
+  const dispatch = useDispatch()
   return (
     <header className='header'>
       <div className='header__logo'>
         <h1>FSR</h1>
       </div>
       <div className='header__search'>
-        <button className='header__button'><BsSearch size={18}/></button>
+        <button
+        onClick={() => dispatch(getSearchPhotos(inputRef.current.value))}
+        className='header__button'><BsSearch size={18}/></button>
         <input
+        ref={inputRef}
+        onKeyUp={(event) => {
+          if (event.key != 'Enter') return;
+          dispatch(getSearchPhotos(inputRef.current.value))
+        }}
         placeholder='Search images'
         className='header__input'
         type='search' />
