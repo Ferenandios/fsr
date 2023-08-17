48a1559be14206a2c9a9e7c01f6bda865a435163
Ferenandios <www.terunplay@gmail.com>
2023-08-17 13:35:31 +0500

    Updated margin parameter

--- src/components/Items/Items.jsx
+++ src/components/Items/Items.jsx
@@ -5,7 +5,7 @@ import Item from '../Item/Item'
 const Items = () => {
   const photos = useSelector(state => state.photos.photos)
   return (
-    <section className='flex flex-col items-center my-10'>
+    <section className='flex flex-col items-center my-[104px]'>
       {photos.map( (data, index) => <Item key={index} data={data} />)}
     </section>
   )
