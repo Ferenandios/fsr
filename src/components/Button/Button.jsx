dff1980038d6843d22cd70e62cb4c85ca30f9f83
Ferenandios <www.terunplay@gmail.com>
2023-08-17 13:32:28 +0500

    Updated dispatch for button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import './Button.css'
 import { useDispatch } from 'react-redux'
-import { getPhotos } from '../../features/photos.slice'
+import { getRandomPhotos } from '../../features/photos.slice'
 
 const Button = () => {
   const dispatch = useDispatch()
   return (
     <div className='flex justify-center mb-10'>
       <button
-      onClick={() => {
-        dispatch(getPhotos())
-      }}
+      onClick={() => dispatch(getRandomPhotos())}
       className='content__button'>Generate new photos</button>
     </div>
   )
