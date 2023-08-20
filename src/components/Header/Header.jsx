4afd78ea9ed766eb5f735b428df19233eba31336
Ferenandios <www.terunplay@gmail.com>
2023-08-20 17:36:40 +0500

    Added container

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,23 +9,28 @@ const Header = () => {
   const dispatch = useDispatch()
   return (
     <header className='header'>
-      <div className='header__logo'>
-        <h1>FSR</h1>
+      <div className="sm:container sm:mx-auto">
+        <div className="header__inner">
+          <div className='header__logo'>
+            <h1>FSR</h1>
+          </div>
+          <div className='header__search'>
+            <button
+              onClick={() => dispatch(getSearchPhotos(inputRef.current.value))}
+              className='header__button'><BsSearch size={18} /></button>
+            <input
+              ref={inputRef}
+              onKeyUp={(event) => {
+                if (event.key != 'Enter') return;
+                dispatch(getSearchPhotos(inputRef.current.value))
+              }}
+              placeholder='Search images'
+              className='header__input'
+              type='search' />
+          </div>
+        </div>
       </div>
-      <div className='header__search'>
-        <button
-        onClick={() => dispatch(getSearchPhotos(inputRef.current.value))}
-        className='header__button'><BsSearch size={18}/></button>
-        <input
-        ref={inputRef}
-        onKeyUp={(event) => {
-          if (event.key != 'Enter') return;
-          dispatch(getSearchPhotos(inputRef.current.value))
-        }}
-        placeholder='Search images'
-        className='header__input'
-        type='search' />
-      </div>
+
     </header>
   )
 }
