04a800981061cc4d7a23bc5263a4700e675857b8
Ferenandios <www.terunplay@gmail.com>
2023-08-26 08:22:16 +0500

    Fixed Responsive Design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/**/*.{jsx,js}"],
   theme: {
+    screens: {
+      "2k": "2030px"
+    },
     extend: {},
     fontFamily: {
       'raleway': ['Raleway', 'sans-serif']
